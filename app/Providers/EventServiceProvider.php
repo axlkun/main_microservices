bd770a50f8e6d84de621c8db0d5e1d572bd15805
axlkun <axelcruz.dev@gmail.com>
2024-03-02 13:07:50 -0600

    add event to create product

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Jobs\ProductCreated;
 use App\Jobs\TestJob;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
@@ -27,6 +28,7 @@ class EventServiceProvider extends ServiceProvider
     public function boot(): void
     {
         \App::bindMethod(TestJob::class . '@handle', fn($job) => $job->handle());
+        \App::bindMethod(ProductCreated::class . '@handle', fn($job) => $job->handle());
     }
 
     /**
