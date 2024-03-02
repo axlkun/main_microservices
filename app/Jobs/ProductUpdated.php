35a13157a9b955f40c78a2d154ae4080615c7c1c
axlkun <axelcruz.dev@gmail.com>
2024-03-02 14:28:37 -0600

    update the register with the data received

--- app/Jobs/ProductUpdated.php
+++ app/Jobs/ProductUpdated.php
@@ -2,11 +2,12 @@
 
 namespace App\Jobs;
 
+use App\Models\Product;
 use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 
 class ProductUpdated implements ShouldQueue
 {
@@ -26,6 +27,13 @@ class ProductUpdated implements ShouldQueue
      */
     public function handle(): void
     {
-        //
+        $product = Product::find($this->data['id']);
+
+        $product->update([
+            'title' => $this->data['title'],
+            'image' => $this->data['image'],
+            'created_at' => $this->data['created_at'],
+            'updated_at' => $this->data['updated_at']
+        ]);
     }
 }
