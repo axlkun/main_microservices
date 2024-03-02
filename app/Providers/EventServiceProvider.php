8a1e7143084b9ffdce9a4003938127985eef358c
axlkun <axelcruz.dev@gmail.com>
2024-03-02 14:21:00 -0600

    add the jobs to listening for

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use App\Jobs\ProductCreated;
+use App\Jobs\ProductDeleted;
+use App\Jobs\ProductUpdated;
 use App\Jobs\TestJob;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
@@ -29,6 +31,8 @@ class EventServiceProvider extends ServiceProvider
     {
         \App::bindMethod(TestJob::class . '@handle', fn($job) => $job->handle());
         \App::bindMethod(ProductCreated::class . '@handle', fn($job) => $job->handle());
+        \App::bindMethod(ProductUpdated::class . '@handle', fn($job) => $job->handle());
+        \App::bindMethod(ProductDeleted::class . '@handle', fn($job) => $job->handle());
     }
 
     /**
