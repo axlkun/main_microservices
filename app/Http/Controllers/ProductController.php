04208c0ee0fcdcce0307772c369ae0bfea07ff74
axlkun <axelcruz.dev@gmail.com>
2024-03-02 21:58:20 -0600

    insert the data to the table

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use Http;
 use App\Models\Product;
+use App\Models\ProductUser;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
@@ -15,6 +17,22 @@ class ProductController extends Controller
     public function like($id, Request $request){
         $response = \Http::get('http://docker.for.mac.localhost:8000/api/user');
 
-        return $response->json();
+        $user = $response->json();
+
+        try{
+            ProductUser::create([
+                'user_id' => $user['id'],
+                'product_id' => $id
+            ]);
+
+            return response([
+                'message' => 'success'
+            ]);
+        }catch(\Exception $exception){
+            return response([
+                'error' => 'You already liked this product!'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+        
     }
 }
